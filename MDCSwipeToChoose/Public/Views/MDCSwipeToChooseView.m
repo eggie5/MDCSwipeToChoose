02570ab12e74a5954d11c51614cd5b34d986cce2
Alex Egg <eggie5@gmail.com>
2014-10-02 16:38:47 -0700

    Update MDCSwipeToChooseView.m

--- MDCSwipeToChoose/Public/Views/MDCSwipeToChooseView.m
+++ MDCSwipeToChoose/Public/Views/MDCSwipeToChooseView.m
@@ -103,6 +103,7 @@ static CGFloat const MDCSwipeToChooseViewLabelWidth = 65.f;
     MDCSwipeOptions *options = [MDCSwipeOptions new];
     options.delegate = self.options.delegate;
     options.threshold = self.options.threshold;
+    options.allowedSwipeDirections=self.options.allowedSwipeDirections;
 
     __block UIView *likedImageView = self.likedView;
     __block UIView *nopeImageView = self.nopeView;
